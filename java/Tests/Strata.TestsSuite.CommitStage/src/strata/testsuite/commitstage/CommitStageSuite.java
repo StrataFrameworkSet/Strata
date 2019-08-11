699e8f3678e741fa8ac5fe8025a10da2957ae70d
John Liebenau <johnliebenau@gmail.com>
2019-08-11 15:50:50 -0700

    Restructuring

--- java/Tests/Strata.TestsSuite.CommitStage/src/strata/testsuite/commitstage/CommitStageSuite.java
+++ java/Tests/Strata.TestsSuite.CommitStage/src/strata/testsuite/commitstage/CommitStageSuite.java
@@ -7,10 +7,11 @@ package strata.testsuite.commitstage;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import strata.foundation.core.utility.CopyableTest;
+import strata.foundation.core.utility.SynchronizerTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    //SynchronizerTest.class,
+    SynchronizerTest.class,
     CopyableTest.class})
 public
 class CommitStageSuite {}
